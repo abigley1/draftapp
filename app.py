d0adf41663f8474441f3b087892b710bf3aa771e
abigley1 <39389017+abigley1@users.noreply.github.com>
2018-11-14 20:30:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def make_tsne(data, inital_player):
 
     data1 = [trace1, trace2]
     layout = go.Layout({'hovermode':'closest', 
-                        'margin':{'t':25, 'r':0},
+                        'margin':{'t':25, 'r':0, 'l':0, 'b':0},
                         'legend':{'x':0, 'y':1},
                        'paper_bgcolor':'#F3F2F4',
                         'plot_bgcolor':'#F3F2F4'})
